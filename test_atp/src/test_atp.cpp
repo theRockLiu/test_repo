97117c7af030474da287dc71e3a8a664d87c2e14
rock <rock@localhost.localdomain>
2016-01-03 20:48:33 +0800

    ok

--- test_atp/src/test_atp.cpp
+++ test_atp/src/test_atp.cpp
@@ -30,15 +30,44 @@ typedef struct price_level_info
 
 class my_algo1
 {
+		typedef void (my_algo1::*my_algo_func)(evt_t*);
+		struct evt_handler
+		{
+				my_algo_func func_;
+		};
 	public:
 		my_algo1()
+												: evt_handlers_( { { &my_algo1::handle_quot } })
 		{
 		}
 		~my_algo1()
 		{
 		}
 
+		void init(satp::quot_engine::pointer_t &qe, satp::trade_engine::pointer_t &te)
+		{
+			dce_qe_ = qe;
+			dce_te_ = te;
+		}
+
 	public:
+
+		void exec()
+		{
+			evt_t* et = dce_qe_->get_evt();
+			if (NULL != et)
+			{
+				(this->*(evt_handlers_[et->id_].func_))(et);
+			}
+
+			et = dce_te_->get_evt();
+			if (NULL != et)
+			{
+				(this->*(evt_handlers_[et->id_].func_))(et);
+			}
+		}
+
+	private:
 		void handle_quot(evt_t* vt)
 		{
 
@@ -53,14 +82,13 @@ class my_algo1
 		{
 
 		}
-};
 
-typedef void (my_algo1::*my_algo_func)(evt_t*);
-struct evt_handler
-{
-		my_algo_func func_;
-		uint8_t evt_id_;
-} evt_handlers[] = { { &my_algo1::handle_quot, EVT_QUOT } };
+	private:
+		satp::quot_engine::pointer_t dce_qe_;
+		satp::trade_engine::pointer_t dce_te_;
+		///satp::trade_engine::pointer_t sq_te_;
+		struct evt_handler evt_handlers_[EVT_CNT];
+};
 
 int main()
 {
@@ -72,25 +100,18 @@ int main()
 	std::vector<std::string> contracts = { "a1603", "a1605" };
 	////
 	satp::quot_engine::pointer_t qe = ap.create_quot_engine(x, contracts);
+	SU_ASSERT(nullptr != qe);
 	satp::trade_engine::pointer_t te = ap.create_trade_engine(x, contracts);
+	SU_ASSERT(nullptr != te);
 	///
 	my_algo1 ma;
+	ma.init(qe, te);
 	evt_t *et = NULL;
 	////now run the engines.
 	ap.start();
 	for (;;)
 	{
-		et = qe->get_evt();
-		if (NULL != et)
-		{
-			(ma.*(evt_handlers[et->id_].func_))(et);
-		}
-
-		et = te->get_evt();
-		if (NULL != et)
-		{
-			(ma.*(evt_handlers[et->id_].func_))(et);
-		}
+		ma.exec();
 	}
 	ap.stop();
 }
